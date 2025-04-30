052dd794f9321b3573cc6d40ae6b78b99faacfca
jaquobia <jaquobia@gmail.com>
2025-04-30 08:51:03 -0400

    dont have commas

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 {
 	pkgs ? import (
 		fetchTarball {
-			url = "https://github.com/NixOS/nixpkgs/archive/8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296.tar.gz",
-			sha256 = "sha256:00000000000",
+			url = "https://github.com/NixOS/nixpkgs/archive/8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296.tar.gz";
+			sha256 = "sha256:00000000000";
 		}
 	) {},
 	...
