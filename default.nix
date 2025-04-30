52477ab7cc9894f342a9e4dec6fa2c635681f9c5
jaquobia <jaquobia@gmail.com>
2025-04-30 08:51:57 -0400

    hash length

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 	pkgs ? import (
 		fetchTarball {
 			url = "https://github.com/NixOS/nixpkgs/archive/8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296.tar.gz";
-			sha256 = "sha256:00000000000";
+			sha256 = "sha256:06jzngg5jm1f81sc4xfskvvgjy5bblz51xpl788mnps1wrkykfhp";
 		}
 	) {},
 	...
