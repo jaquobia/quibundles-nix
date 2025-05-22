002fe43c33677801a1ef2a44e23c598f55231188
jaquobia <jaquobia@gmail.com>
2025-05-22 19:05:56 -0400

    Make some changes to how packages are collected

--- flake.nix
+++ flake.nix
@@ -9,30 +9,17 @@
 
 	outputs = { self, nixpkgs, ... }:
 		let
-			lib = nixpkgs.lib;
-			forAllSystems = lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+			forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 		in 
 		{
-			packages = forAllSystems(
-				system:
-				let
-						pkgs = import nixpkgs {
-								inherit system;
-								config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-								"fmod"
-								];
-						
-						};
-						doomseeker-pkg = pkgs.libsForQt5.callPackage ./pkgs/doomseeker { };
-						zandronum-pkg = pkgs.callPackage ./pkgs/zandronum { };
-						sm64coopdx-pkg = pkgs.callPackage ./pkgs/sm64coopdx { };
-				in
-				{
-					default = doomseeker-pkg;
-					doomseeker = doomseeker-pkg;
-					zandronum = zandronum-pkg;
-					sm64coopdx = sm64coopdx-pkg;
-				}
-			);
+			lib = {
+				pkgsFor = pkgs: pkgs.callPackage ./pkgs { };
+				# {
+				# 	doomseeker = pkgs.libsForQt5.callPackage ./pkgs/doomseeker { };
+				# 	zandronum = pkgs.callPackage ./pkgs/zandronum { };
+				# 	sm64coopdx = pkgs.callPackage ./pkgs/sm64coopdx { };
+				# };
+			};
+			packages = forAllSystems(system: self.lib.pkgsFor nixpkgs.legacyPackages.${system});
 		};
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,6 @@
-{
-	pkgs ? import <nixpkgs> { },
-	...
-}:
+{ pkgs }:
 {
 	doomseeker = pkgs.libsForQt5.callPackage ./pkgs/doomseeker { };
 	zandronum = pkgs.callPackage ./pkgs/zandronum { };
+	sm64coopdx = pkgs.callPackage ./pkgs/sm64coopdx { };
 }
