0783b51d088d42bb775070ef778f56e193230ff3
jaquobia <jaquobia@gmail.com>
2025-05-22 19:31:06 -0400

    Fix paths for callPackage

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,6 @@
 { pkgs }:
 {
-	doomseeker = pkgs.libsForQt5.callPackage ./pkgs/doomseeker { };
-	zandronum = pkgs.callPackage ./pkgs/zandronum { };
-	sm64coopdx = pkgs.callPackage ./pkgs/sm64coopdx { };
+	doomseeker = pkgs.libsForQt5.callPackage ./doomseeker { };
+	zandronum = pkgs.callPackage ./zandronum { };
+	sm64coopdx = pkgs.callPackage ./sm64coopdx { };
 }
