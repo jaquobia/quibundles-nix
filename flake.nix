1e71bd017f186f46f48c4cdbcbe2fe5d478a45e5
jaquobia <jaquobia@gmail.com>
2025-05-22 19:37:26 -0400

    Remove some commented out code

--- flake.nix
+++ flake.nix
@@ -14,11 +14,6 @@
 		{
 			lib = {
 				pkgsFor = pkgs: pkgs.callPackage ./pkgs { };
-				# {
-				# 	doomseeker = pkgs.libsForQt5.callPackage ./pkgs/doomseeker { };
-				# 	zandronum = pkgs.callPackage ./pkgs/zandronum { };
-				# 	sm64coopdx = pkgs.callPackage ./pkgs/sm64coopdx { };
-				# };
 			};
 			packages = forAllSystems(system: self.lib.pkgsFor nixpkgs.legacyPackages.${system});
 		};
